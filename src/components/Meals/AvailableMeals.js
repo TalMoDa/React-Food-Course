8845ed0378aa2a393f8e99467bacc219d47dabe0
Tal Dayan <80027634+TalMoDa@users.noreply.github.com>
2022-05-26 10:46:28 +0300

    react

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,41 +1,58 @@
 import classes from "./AvailableMeals.moudule.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
+import {useEffect, useState} from "react";
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
 
-const AvailableMeals = () => {
-    const  mealsList = DUMMY_MEALS.map(meal => <MealItem
+
+const AvailableMeals =  () => {
+    const [apiMealList,SetApiMealList] = useState([]);
+    const [isLoading,setIsLoading] = useState(false);
+    const [apiError, setApiError] = useState(null);
+
+    useEffect(() => {
+        setIsLoading(true);
+        const fetchMeals = async () => {
+            const results = await fetch('https://movies-ee0e6-default-rtdb.firebaseio.com/meals.json');
+            if(!results.ok){
+                throw new Error('something went wrong!');
+            }
+            const resultsData = await results.json();
+
+            const  loadedMeals = [];
+
+            for(const key in resultsData){
+                loadedMeals.push({
+                    id: key,
+                    name: resultsData[key].name,
+                    description: resultsData[key].description,
+                    price: resultsData[key].price
+                });
+            }
+            SetApiMealList(loadedMeals);
+            setIsLoading(false);
+        };
+             fetchMeals().catch( error =>{
+                 setIsLoading(false);
+                 setApiError(error.message);
+                 }
+             );
+             },[]);
+    if(apiError){
+        return (<section className={classes.MealsError}><p>{apiError}</p></section>)
+    }
+    if(isLoading){
+        return (<section className={classes.MealsLoading}><p>loading.....</p></section>)
+    }
+
+    const  mealsList = apiMealList.map(meal => <MealItem
         id={meal.id}
         key={meal.id}
         name={meal.name}
         price={meal.price}
         description={meal.description} />);
+
+
 
     return <section className={classes.meals}>
         <Card>
